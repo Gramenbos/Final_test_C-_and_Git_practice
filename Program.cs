108522551c941af628c8a13a64b23217070a47f9
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-02-16 22:34:23 +0300

    Created FillArray method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] workArray = FillArray();
+Console.WriteLine(PrintArray(workArray));
+int counter = CountStringSymbols(workArray);
+Console.WriteLine(counter);
+
+string[] FillArray()
+{
+    Console.WriteLine("Enter any strings separated by commas and/or spaces: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
+
